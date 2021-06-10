4ab2cf8d907ddf076eb3d21e0d920595bebbdfc5
Ryan Murdoch <rmurdoch@lucidsolutions.tech>
2021-06-10 22:16:15 +0100

    Setup Tests Folder and Imported NSubstitute

---
+++ Assets/Tests/RestrainedVerticalMovementControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class RestrainedVerticalMovementControllerTests
+{
+    [Test] 
+    public void Move_Object_Up() 
+    {
+        
+    }
+
+    [Test] 
+    public void Move_Object_Down()
+    {
+        
+    }
+
+    [Test] 
+    public void Restrain_Upward_Movement()
+    {
+        
+    }
+
+    [Test] 
+    public void Restrain_DownwardMovement()
+    {
+        
+    }
+}
